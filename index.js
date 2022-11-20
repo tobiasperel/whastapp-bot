9328a3cac55ae09a91e3e436874cbd198f7502e3
TobiPerel <pereltobias@gmail.com>
2022-11-20 14:44:06 -0300

    nube

--- index.js
+++ index.js
@@ -10,24 +10,26 @@ const client = new Client({
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
 });
-const number = "1111111111111@c.us"
-const text = "Hola ma!";
+const number = "5491126147226@c.us"
+const text = "Hola, te has registrado correctamente en el sistema de notificaciones de la Municipalidad de San Miguel de Tucumán. Para confirmar tu suscripción, por favor, envía la palabra 'confirmar'";
 
 client.on('ready', () => {
     console.log('Client is ready!');
-    
-    client.sendMessage(number, text);
-});
 
+    //client.sendMessage(number, text);
+});
+let seMando = false;
 client.on('message', message => {
-    console.log(message.from, message.body, );
+    console.log(message.from, message.body);
     if (message.body === 'ping') {
         message.reply('pong');
     }
-    if(message.from === number){
-        for(let i = 0; i < 10; i++){
-            message.reply('pong');
-        }
+    if(message.from == number && seMando == false){
+        seMando = true;
+        message.reply(text)
+    }
+    if(message.from == number && message.body.toLowerCase() == "confirmar"){
+        message.reply("Gracias por confirmar tu suscripción. Ahora recibirás las notificaciones de la Municipalidad de San Miguel de Tucumán.")
     }
 });
 
